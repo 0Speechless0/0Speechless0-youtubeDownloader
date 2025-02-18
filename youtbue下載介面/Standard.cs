5147a4e6531936b23b0f65693b5baf933651ec19
aedre <aedre@trillioninnovations.com>
2025-02-18 12:17:32 +0800

    調整Util

---
+++ youtbue下載介面/Standard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace youtbue下載介面
+{
+    internal static class Standard
+    {
+        public static int UrlTypeCheck(this string urlEnd) => urlEnd switch
+        {
+            "watch" => 1,
+            "playlist" => 2,
+            _ => 0
+        };
+        public static string downLoadTypeCheck(this string downloadType) => downloadType switch
+        {
+            "video" => "video",
+            "audio" => "audio",
+            _ => ""
+        };
+
+
+
+
+    }
+}
--- youtbue下載介面/Util.cs
+++ youtbue下載介面/Util.cs
@@ -11,18 +11,7 @@ namespace youtbue下載介面
 {
     internal static class Util 
     {
-        public static int UrlTypeCheck(this string urlEnd) => urlEnd switch
-        {
-            "watch" => 1,
-            "playlist" => 2,
-            _ => 0
-        };
-        public static string downLoadTypeCheck(this string downloadType) => downloadType switch
-        {
-            "video" => "video",
-            "audio" => "audio",
-            _ => ""
-        };
+
 
 
         public static int getPlayListItemCount(this string listCode)
